7768e5026969dd53ca933f057249e49605aa504e
Documenter.jl <documenter@juliadocs.github.io>
2021-07-03 19:30:49 +0000

    build based on d5da1af

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = InstaRound","category":"page"},{"location":"#InstaRound","page":"Home","title":"InstaRound","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for InstaRound.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [InstaRound]","category":"page"}]
+}
